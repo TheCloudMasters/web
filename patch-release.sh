62b1c180f91c5a0e6ff4cff2cab5e5185443f90c
Zach McElrath <zach@ues.io>
2023-07-25 16:36:45 -0600

    what is wrong with grep

--- patch-release.sh
+++ patch-release.sh
@@ -2,15 +2,20 @@
 
 set -e
 
-uesio login
-uesio work
-npm run push
-echo "Seeding workspace with data..."
-./seed.sh
-echo "Creating new patch bundle..."
-patchResult=$(uesio bundle create -t=patch)
+# uesio login
+# uesio work
+# npm run push
+# echo "Seeding workspace with data..."
+# ./seed.sh
+# echo "Creating new patch bundle..."
+# patchResult=$(uesio bundle create -t=patch)
+patchResult="patch result is Successfully created new bundle version: 1.0.3"
 echo "patch result is $patchResult"
-version=$(echo "$patchResult" | grep -o "\d\+.\d\+.\d\+")
+#version=$(echo "$patchResult" | grep -o "\d\+.\d\+.\d\+")
+version=$(grep -o "\d\+.\d\+.\d\+" <<< $patchResult)
+echo "version is $version"
+exit 1
+
 echo "Created bundle with version = $version"
 echo "Deploying patch bundle to site 'prod'..."
 uesio siteadmin -n=prod
