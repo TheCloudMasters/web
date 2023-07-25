926c0ac954b359ac2e1da02c9c1638e0cd8694b4
Zach McElrath <zach@ues.io>
2023-07-25 16:28:17 -0600

    ci: Move patch release into dedicated script

---
+++ patch-release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+uesio login
+npm run push
+echo "Seeding workspace with data..."
+./seed.sh
+echo "Creating new patch bundle..."
+patchResult=$(uesio bundle create -t=patch)
+version=$(echo "$patchResult" | grep -o "\d\+.\d\+.\d\+")
+echo "Created bundle with version = $version"
+echo "Deploying patch bundle to site 'prod'..."
+uesio siteadmin -n=prod
+uesio site use -b=$version
+echo "Uploading latest docs to site 'prod'..."
+./seedprod.sh
+echo "Successfully released latest docs!"
