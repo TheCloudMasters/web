4bf81cdd57a020638b6d9c0d1e155051caf4f851
Zach McElrath <zach@ues.io>
2023-11-17 09:30:36 -0500

    ci: Get Github branch deployments working

--- patch-release.sh
+++ patch-release.sh
@@ -3,20 +3,24 @@
 set -e
 
 uesio login
-uesio work
-npm run push
-echo "Seeding workspace with data..."
+uesio work -n=$WORKSPACE
+uesio pack && uesio workspace deploy
+
+echo "Uploading latest blog posts to workspace '$WORKSPACE'..."
 ./seed.sh
+echo "Successfully uploaded latest blog posts to workspace."
+
 echo "Creating new patch bundle..."
-patchResult=$(uesio bundle create -t=patch)
+patchResult=$(uesio bundle create -t=patch -d="$DESCRIPTION")
 
 # Extract version number from patchResult variable using grep
 version=$(echo "$patchResult" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
 
 echo "Created bundle with version = $version"
-echo "Deploying patch bundle to site 'prod'..."
-uesio siteadmin -n=prod
+echo "Deploying patch bundle to site '$SITE'..."
+uesio siteadmin -n=$SITE
 uesio site use -b=$version
-echo "Uploading latest blog posts to site 'prod'..."
-./seedprod.sh
+
+echo "Uploading latest blog posts to site '$SITE'..."
+./seedsite.sh
 echo "Successfully released latest blog posts!"
