21c5fed9a3eeedc8bf844dedeee4cd4025ffcf5b
MarkoStrassl <123621483+MarkoStrassl@users.noreply.github.com>
2023-12-12 11:59:56 +0100

    Managed package campaign (#24)
    * more content
    
    * latest changes
    
    * prettier
    
    * prettier
    
    * feedback
    
    * change 33 to 35

--- bundle/bots/listener/request/bot.ts
+++ bundle/bots/listener/request/bot.ts
@@ -4,6 +4,7 @@ function request(bot: ListenerBotApi) {
 		"first_name",
 		"last_name",
 		"email",
+		"phone",
 		"company",
 		"no_employees",
 		"location",
@@ -19,28 +20,13 @@ function request(bot: ListenerBotApi) {
 		{}
 	) as Record<string, string>
 
-	const labels = {
-		first_name: "first name",
-		last_name: "last name",
-		email: "email",
-		description: "description",
-	} as Record<string, string>
-
-	// for (const key in values) {
-	// 	if (
-	// 		values[key] === "" &&
-	// 		values[key] !== "no_employees" &&
-	// 		values[key] !== "company"
-	// 	)
-	// 		throw new Error(`missing ${labels[key]}`)
-	// }
-
 	// Save the lead in our leads collection
 	bot.asAdmin.save("uesio/crm.lead", [
 		{
 			"uesio/crm.firstname": values.first_name,
 			"uesio/crm.lastname": values.last_name,
 			"uesio/crm.email": values.email,
+			"uesio/crm.phone_business": values.phone,
 			"uesio/crm.account_name": values.company,
 			"uesio/crm.no_employees": Number(values.no_employees),
 			"uesio/crm.location": values.country,
@@ -55,18 +41,32 @@ function request(bot: ListenerBotApi) {
 
 	// Send an email to the user
 	const salesEmail = bot.asAdmin.getConfigValue("uesio/crm.sales_email")
-	const userConfigValue =
-		values["topic"] === "Contact Request"
-			? "uesio/web.email_template_contact_to_user"
-			: "uesio/web.email_template_demo_to_user"
-
-	const SalesConfigValue =
-		values["topic"] === "Contact Request"
-			? "uesio/web.email_template_contact_to_sales"
-			: "uesio/web.email_template_demo_to_sales"
+	let userConfigValue = ""
+	let salesConfigValue = ""
+	switch (values["topic"]) {
+		case "contact request": {
+			userConfigValue = "uesio/web.email_template_contact_to_user"
+			salesConfigValue = "uesio/web.email_template_contact_to_sales"
+			break
+		}
+		case "demo request": {
+			userConfigValue = "uesio/web.email_template_contact_to_sales"
+			salesConfigValue = "uesio/web.email_template_demo_to_sales"
+			break
+		}
+		case "managed package": {
+			userConfigValue = "uesio/web.email_template_contact_to_user"
+			salesConfigValue =
+				"uesio/web.email_template_managed_package_to_sales"
+			break
+		}
+		default: {
+			break
+		}
+	}
 
 	const templateIdUser = bot.asAdmin.getConfigValue(userConfigValue)
-	const templateIdSales = bot.asAdmin.getConfigValue(SalesConfigValue)
+	const templateIdSales = bot.asAdmin.getConfigValue(salesConfigValue)
 
 	// // Email to user
 	bot.asAdmin.runIntegrationAction("uesio/crm.sendgrid", "sendemail", {
@@ -94,6 +94,7 @@ function request(bot: ListenerBotApi) {
 			firstname: values["first_name"],
 			lastname: values["last_name"],
 			email: values["email"],
+			phone: values["phone"],
 			account: values["company"],
 			no_employees: values["no_employees"],
 			country: values["location"],
