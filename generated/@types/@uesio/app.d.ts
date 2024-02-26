45b9de7f3cdc765f0de3be98e19bfed267fd7992
Gregg Baxter <gregg@baxterweb.ch>
2024-02-26 16:28:38 +0100

    Managed packages chage (#30)
    * Update Managed Packs
    
    * Upadted from rerieve

---
+++ generated/@types/@uesio/app.d.ts
@@ -0,0 +1,27 @@
+
+declare module "@uesio/app/bots/listener/uesio/web/createlogin" {
+
+	type Params = {
+		username: string
+		email: string
+		code: string
+		host: string
+	}
+
+	export type {
+		Params
+	}
+}
+declare module "@uesio/app/bots/listener/uesio/web/forgotpassword" {
+
+	type Params = {
+		username: string
+		email: string
+		code: string
+		host: string
+	}
+
+	export type {
+		Params
+	}
+}
