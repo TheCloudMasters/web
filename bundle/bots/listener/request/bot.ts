9aaeb1daf1ab410d59ce96335686e25af0c477d3
humandad <55447225+humandad@users.noreply.github.com>
2024-06-12 14:11:35 -0400

    remove old crm package

--- bundle/bots/listener/request/bot.ts
+++ bundle/bots/listener/request/bot.ts
@@ -20,27 +20,10 @@ function request(bot: ListenerBotApi) {
 		{}
 	) as Record<string, string>
 
-	// Save the lead in our leads collection
-	bot.asAdmin.save("uesio/crm.lead", [
-		{
-			"uesio/crm.firstname": values.first_name,
-			"uesio/crm.lastname": values.last_name,
-			"uesio/crm.email": values.email,
-			"uesio/crm.phone_business": values.phone,
-			"uesio/crm.account_name": values.company,
-			"uesio/crm.no_employees": Number(values.no_employees),
-			"uesio/crm.location": values.country,
-			"uesio/crm.description": values.description,
-			"uesio/crm.topic": values.topic,
-			"uesio/crm.lead_source": "ues.io website",
-			"uesio/crm.status": "OPEN",
-		} as unknown as WireRecord,
-	])
-
 	// return
 
 	// Send an email to the user
-	const salesEmail = bot.asAdmin.getConfigValue("uesio/crm.sales_email")
+	const salesEmail = bot.asAdmin.getConfigValue("uesio/web.sales_email")
 	let userConfigValue = ""
 	let salesConfigValue = ""
 	switch (values["topic"]) {
@@ -63,7 +46,7 @@ function request(bot: ListenerBotApi) {
 	const templateIdSales = bot.asAdmin.getConfigValue(salesConfigValue)
 
 	// // Email to user
-	bot.asAdmin.runIntegrationAction("uesio/crm.sendgrid", "sendemail", {
+	bot.asAdmin.runIntegrationAction("uesio/core.sendgrid", "sendemail", {
 		to: [values.email],
 		from: salesEmail,
 		templateId: templateIdUser,
@@ -80,7 +63,7 @@ function request(bot: ListenerBotApi) {
 	})
 
 	// // Email to us
-	bot.asAdmin.runIntegrationAction("uesio/crm.sendgrid", "sendemail", {
+	bot.asAdmin.runIntegrationAction("uesio/core.sendgrid", "sendemail", {
 		to: [salesEmail],
 		from: salesEmail,
 		templateId: templateIdSales,
